c2ded2a54df12edf915fb1b69059907b82aeef95
vytao06012003 <vytao.06012003@gmail.com>
2025-08-26 16:26:39 +0700

    update dockerfile

--- start.sh
+++ start.sh
@@ -3,8 +3,16 @@
 # Init DB metadata nếu chưa có
 airflow db init
 
+# Tạo user admin (chỉ tạo nếu chưa tồn tại)
+airflow users create \
+    --username admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --email admin@example.com || true
+
 # Khởi chạy Scheduler ở background
 airflow scheduler &
 
 # Khởi chạy Webserver (UI) ở foreground
-exec airflow webserver --port $PORT
+exec airflow webserver --host 0.0.0.0 --port $PORT
